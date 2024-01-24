fc9904c67b0601c91791355a10ecff5c5c5285a7
NickMatthews-1 <matthews3102000@gmail.com>
2024-01-24 20:46:54 +1100

    Created

--- W9U23-10-Stable.sh
+++ W9U23-10-Stable.sh
@@ -17,7 +17,7 @@ sudo apt update
 #This upgrades the packages
 sudo apt upgrade
 
-#This installs wine version 8
+#This installs Wine version 9
 sudo apt install --install-recommends winehq-stable
 
-#Subject to GPLv3 License Copyright Nicolas Matthews 2023
+#Subject to GPLv3 License Copyright Nicolas Matthews 2024
