9f70b526c5b525ab1d74d136b606a77180461128
NickMatthews-1 <matthews3102000@gmail.com>
2024-01-24 20:53:08 +1100

    Created

--- W9U20-Stable.sh
+++ W9U20-Stable.sh
@@ -9,7 +9,7 @@ sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 
 #This downloads the WineHQ sources file
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
 
 #This updates the package information
 sudo apt update
@@ -17,10 +17,7 @@ sudo apt update
 #This upgrades the packages
 sudo apt upgrade
 
-#This installs wine version 7
+#This installs Wine version 9
 sudo apt install --install-recommends winehq-stable
 
-#This may not actually install as at the time of creation there was NO stable version of Wine for Ubuntu 22 platforms.
-#Wine7.0 came out before Jammy Jellyfish and Kinetic Kudu
-
-#Subject to GPLv3 License Copyright Nicolas Matthews 2022
+#Subject to GPLv3 License Copyright Nicolas Matthews 2024
